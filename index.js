deccc296f54f062503c5369da2179a68c8cfb101
123 <1921086998@qq.com>
2023-12-31 15:35:13 +0800

    git stash demo

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 test-demo 
 // thank you
+
+// 在master分支中修改代码
+function fn(){
+    console.log('master');
+}
+
+// 然后尝试切换分支
+// 切换到 testb
+// 会提示你要先commit代码，再可以切换
+
+// 此时又不想提交，或者没开发完成代码...
+// 就可以使用 git stash 了
